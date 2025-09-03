cdd967e47e1030a12e525078522ba0530d302a51
Mar <marbou96@hotmail.com>
2025-09-03 13:18:36 +0200

    las commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,8 +8,7 @@ exports.getProducts = (req, res, next) => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
-        path: '/products',
-        isAuthenticated: req.isLoggedIn
+        path: '/products'
       });
     })
     .catch(err => {
@@ -24,8 +23,7 @@ exports.getProduct = (req, res, next) => {
       res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
-        path: '/products',
-        isAuthenticated: req.isLoggedIn
+        path: '/products'
       });
     })
     .catch(err => console.log(err));
@@ -37,8 +35,7 @@ exports.getIndex = (req, res, next) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/',
-        isAuthenticated: req.isLoggedIn
+        path: '/'
       });
     })
     .catch(err => {
@@ -55,8 +52,7 @@ exports.getCart = (req, res, next) => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: products,
-        isAuthenticated: req.isLoggedIn
+        products: products
       });
     })
     .catch(err => console.log(err));
@@ -94,7 +90,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user
         },
         products: products
@@ -116,8 +112,7 @@ exports.getOrders = (req, res, next) => {
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
-        orders: orders,
-        isAuthenticated: req.isLoggedIn
+        orders: orders
       });
     })
     .catch(err => console.log(err));
